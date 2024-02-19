4a9574e91e0295fae30897dca68d7793148d77d2
sreloumello <sre.luan@gmail.com>
2024-02-19 16:28:04 -0300

    day 1 and 2

---
+++ Day1/band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator!")
+color = input("What's your favourite color? ")
+pet = input("What's your pet's name? ")
+print("Your band name could be " + color + " " + pet)
